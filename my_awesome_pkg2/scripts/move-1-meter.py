9f2186f11e251c266c8a1eff2e88c393d2190b75
beranger <beranger@scherpereel.com>
2021-12-17 14:15:55 +0100

    gogogog

--- my_awesome_pkg2/scripts/move-1-meter.py
+++ my_awesome_pkg2/scripts/move-1-meter.py
@@ -2,7 +2,7 @@
 import math, rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-
+cmd=Twist()
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
 
@@ -12,19 +12,33 @@ commandPublisher = rospy.Publisher(
 )
 
 # Suscribe laser:
-def laser_scan():
-    rospy.Subscriber('/scan', LaserScan, callback)
-    rospy.spin()
+rospy.init_node('laser_scan',anonymous=True)
+rospy.Subscriber('/scan', LaserScan, move_command)
 
 # Publish velocity commandes:
 def move_command(data):
     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
-    cmd= Twist()
+    global cmd
     cmd.linear.x= 0.1
+    cmd.angular.z= 0.0
+    if data.ranges[360] > 0.5:
+      cmd.linear.x = 0.1
+      cmd.angular.z = 0.0
+    else :
+        cmd.linear.x = 0
+        rospy.Timer(rospy.Duration(2), turn(cmd), oneshot=True)
+
+
+
+    commandPublisher.publish(cmd)
+
+def turn(data):
+    global cmd
+    cmd.angular.z =0.2
     commandPublisher.publish(cmd)
 
 # call the move_command at a regular frequency:
-rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
+rospy.Timer( rospy.Duration(0.1), turn, oneshot=False )
 
 # spin() enter the program in a infinite loop
 print("Start move.py")

14c72161be911e71f1c6f280a740e78790afc306
bot@deepthrought <imt-mobisyst@drods.net>
2022-01-10 17:36:32 +0100

    vision lundi

--- groupe-violet/scripts/vision.py
+++ groupe-violet/scripts/vision.py
@@ -184,7 +184,7 @@ for i in range(0, len(YPred)):
     show_img(sample_test_data, test_label, meta, i, label_fn=pred_label_fn)
 """
 
-#======================DETECTION PAR ONDELETTES HAAR============================
+"""#======================DETECTION PAR ONDELETTES HAAR============================
 
 def detectAndDisplay(frame):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -192,7 +192,7 @@ def detectAndDisplay(frame):
 
     #-- Detect cans
     cans = can_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
+    for (x,y,w,h) in cans:
         center = (x + w//2, y + h//2)
         frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (0, 0, 255), 4)
 
@@ -214,4 +214,45 @@ while True:
     if cv2.waitKey(1)&0xFF==ord('q'):
         break
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
+"""
+#======================DETECTION PAR ONDELETTES HAAR2============================
+from __future__ import print_function
+import cv2 as cv
+import argparse
+def detectAndDisplay(frame):
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+    #-- Detect faces
+    cans = cans_cascade.detectMultiScale(frame_gray)
+    for (x,y,w,h) in cans:
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h,x:x+w]
+        
+    cv.imshow('Capture - Face detection', frame)
+parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='/home/bot/catkin_ws/src/LARM-violet/groupe-violet/data/classifier/cascade.xml')
+parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+args = parser.parse_args()
+face_cascade_name = args.face_cascade
+face_cascade = cv.CascadeClassifier()
+#-- 1. Load the cascades
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
+
+camera_device = args.camera
+#-- 2. Read the video stream
+cap = cv.VideoCapture(camera_device)
+if not cap.isOpened:
+    print('--(!)Error opening video capture')
+    exit(0)
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        print('--(!) No captured frame -- Break!')
+        break
+    detectAndDisplay(frame)
+    if cv.waitKey(10) == 27:
+        break

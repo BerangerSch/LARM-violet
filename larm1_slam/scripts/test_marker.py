3c4b41e751885986cb7bc4f0ddaac6f44c297142
Beranger <beranger.scherpereel@etu.imt-nord-europe.fr>
2022-01-12 12:11:16 +0100

    distance objet

--- larm1_slam/scripts/test_marker.py
+++ larm1_slam/scripts/test_marker.py
@@ -6,8 +6,8 @@ rospy.init_node('bottle', anonymous=True)
 
 rate = rospy.Rate(10)
 
-publisher = rospy.Publisher('/bottle', geo_msgs.Vector3, queue_size=1)
-coord = geo_msgs.Vector3(0, 0, 0.5)
+publisher = rospy.Publisher('/bottle', tuple, queue_size=1)
+coord = (200, 120)
 while not rospy.is_shutdown():
     publisher.publish(coord)
     rate.sleep()
